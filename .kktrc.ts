e11021d1a1ee172b69784880a24161742a2da935
jaywcjlove <398188662@qq.com>
2022-01-10 13:13:17 +0800

    website: update kkt config.

--- .kktrc.ts
+++ .kktrc.ts
@@ -21,6 +21,33 @@ export default (conf: Configuration, env: 'production' | 'development', options:
   }));
   if (env === 'production') {
     conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          refractor: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-prismjs-vendor',
+            chunks: 'all',
+          },
+          runtime: {
+            test: /[\\/]node_modules[\\/](@babel)[\\/]/,
+            name: 'babel-vendor',
+            chunks: 'all',
+          },
+          parse5: {
+            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+            name: 'parse5-vendor',
+            chunks: 'all',
+          },
+        }
+      }
+    }
   }
   return conf;
 }
