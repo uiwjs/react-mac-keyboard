304ab36ee7a6b5fa85711563611814797eef58d1
jaywcjlove <398188662@qq.com>
2023-04-04 16:55:25 +0800

    chore(deps): update react monorepo to v18 (major) #84

--- website/index.tsx
+++ website/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container!);
+root.render(<App />);
